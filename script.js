520915d5db53b9ec9baf31b587099080267a95d4
rythroja <jared@mercenarytricking.com>
2023-10-18 12:54:02 -0700

    replace regex with ops constant

--- script.js
+++ script.js
@@ -10,13 +10,18 @@ let aExpression;
 let bExpression;
 let firstChar;
 
+
 const plus = '+';
 const minus = '-';
 const times = 'x';
 const dividedBy = '÷'; 
-const ops = '[/+]|x|-|÷';
+// const ops = '[/+]|x|-|÷';
+const ops = /\+|-|x|÷/g;
+const opArraytions = ['+', '-', 'x', '÷'];
 const zeroErrorMessage = `ERROR, divide by zero`;
 
+
+  
 // numbers 
 const oneButton = document.querySelector('#one');
     oneButton.addEventListener('click', () => addInput(1));
@@ -73,10 +78,7 @@ const addButton = document.querySelector('#add');
     addButton.addEventListener('click', ()=> operation(plus));
 
 
-
-
-
-        
+         
 
 // calculations 
 function round(num){
@@ -114,22 +116,77 @@ function operation(e){
     currentOperator = e;
     addInput(e);
 }
-console.log('hello')
+
 function addInput(e) {
     if(inputDisplay.innerText.length > 25)
         console.log ('error');
-    // if ((e === '%' 
+    
+    // if (e === '%' 
     //     && (inputDisplay.innerHTML !== ""
     //     || !inputDisplay.innerHTML.includes('/+|-|x|÷|%/'))
-    //     )){
-    //         console.log('percent time')
+    //     ){
+    //         console.log(inputDisplay.innerText);
+    //         console.log(bExpression);
     //     return;
     // }
+
+    // Check for zero 
+    // check for dot
+    if(e === '.'){
+        addPoint();
+        return;
+    }
+    if(inputDisplay.innerText === '0'){
+        inputDisplay.innerText = e;
+        return;
+    }
+    // append selected input 
     else{
-        inputDisplay.innerText = `${inputDisplay.innerText}${e}`;
+        inputDisplay.innerText += e;
+        return;
     }
 }
 
+function addPoint(){
+    let thereIsSymbol = (inputDisplay.innerText.match(ops) || []).length;
+    let lastIsSymbol = (inputDisplay.innerText.slice(-1).match(ops) || []).length;
+    let thereIsPoint = (inputDisplay.innerText.match(/\./g) || []).length;
+    // check if empty 
+    if (inputDisplay.innerText.length === 0){
+        inputDisplay.innerText = '0.';
+        console.log('empty')
+        return;
+
+    // check no symbol, single point
+    }else if(thereIsSymbol === 0
+        && thereIsPoint > 0 && thereIsPoint < 2){
+        console.log('no symbol, single point')
+        return;
+    
+    // check for symbol, last is a symbol, and a single point 
+    }else if (thereIsSymbol === 1
+        && lastIsSymbol === 1
+        && thereIsPoint > 0 && thereIsPoint < 2){
+        inputDisplay.innerText += '0.';
+        console.log('symbol, last is symbol, single point')
+        return;
+    // check for symbol, last is not a symbol, and a single point
+    }else if(thereIsSymbol === 1
+        && lastIsSymbol === 0
+        && thereIsPoint > 0 && thereIsPoint < 2){
+        inputDisplay.innerText += '.';
+        console.log('symbol, last is not symbol, single point')
+    // check for symbol, last is not a symbol, and two points
+    }else if(thereIsSymbol === 1
+        && lastIsSymbol === 0
+        && thereIsPoint === 2){
+        console.log('symbol, last not symbol, 2 points')
+        return;
+    }else {
+        inputDisplay.innerText += '.';
+        console.log('else we add .')
+    } 
+} 
     
 
 function calculate(){
@@ -137,6 +194,12 @@ function calculate(){
     if (inputDisplay.innerText.length < 1) {
         return};
     
+    if (currentOperator ==='%'
+        && (inputDisplay.innerHTML !== ""
+        || !inputDisplay.innerHTML.includes('/+|-|x|÷|%/'))){
+
+        return;
+    }
     if (currentOperator === '%'){
             inputDisplay.innerText = product(inputDisplay.innerText.slice(0,-1), 0.01);
             return;
